97a517faf0618b7a4c5d3f720a5ba5eb63bca3b7
Alvaro Ruiz <alvaroruizponce@gmail.com>
2025-03-21 00:37:41 -0300

    more test, integers, strings, arrays and slices

--- arrays/sum.go
+++ arrays/sum.go
@@ -7,3 +7,23 @@ func sum(numbers []int) int {
 	}
 	return sum
 }
+
+//func sumAll(numbersToSum ...[]int) []int {
+/*lenghtOfNumbers := len(numbersToSum)
+sums := make([]int, lenghtOfNumbers)
+
+for i := 0; i < lenghtOfNumbers; i++ {
+	sums[i] = sum(numbersToSum[i])
+}
+return sums*/
+//}
+
+func sumAll(numbersToSum ...[]int) []int {
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, sum(numbers))
+	}
+
+	return sums
+}
